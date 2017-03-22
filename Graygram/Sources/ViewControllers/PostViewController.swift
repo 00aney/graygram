202908e773fdbe5c0ec4ea743db909786d3daa66
aney <yenafirst91@gmail.com>
2017-03-22 22:22:19 +0900

    PostViewController 프로퍼티 추가

--- Graygram/Sources/ViewControllers/PostViewController.swift
+++ Graygram/Sources/ViewControllers/PostViewController.swift
@@ -10,4 +10,27 @@ import UIKit
 
 final class PostViewController: UIViewController {
   
+  fileprivate let post: Post
+  fileprivate let collectionView = UICollectionView()
+  
+  init(post: Post) {
+    self.post = post
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // 미션: 콜렉션 뷰에 PostCardCell이 보이게 해보세요.
+    // 힌트:
+    //  1. 콜렉션 뷰가 화면에 보이도록 함.
+    //  2. DataSource에서 아이템 개수와 셀 인스턴스 반환
+    //  3. Delegate에서 셀 크기 반환
+    
+  }
+  
 }
